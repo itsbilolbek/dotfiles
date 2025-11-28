6558cc7dd20b6288d6fb47e171508eab71c59e2f
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-11-28 17:16:31 +0900

    Change packages list - Add vim-gtk3 for clipboard support

--- setup.sh
+++ setup.sh
@@ -4,19 +4,15 @@
 echo "Installing dependencies..."
 sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install -y \
-     curl \
-     git \
-     tmux \
-     vim \
-     python3 \
-     python3-pip \
-     emacs \
-     org-mode-doc \
-     okular \
-     syncthing \
-     kdeconnect
+declare -a _packages=(
+	git curl vim-gtk3
+	python3 python3-pip
+	neofetch build-essential
+)
 
+for i in "${_packages[@]}"; do
+	sudo apt install -y "$i"
+done
 
 echo "Setting up dotfiles..."
 
@@ -32,26 +28,7 @@ fi
 ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 
 # Repeat for other files
-ln -sf ~/.dotfiles/emacs/init.el ~/.emacs
 ln -sf ~/.dotfiles/.xprofile ~/.xprofile
 
 echo "Dotfiles setup complete!"
 
-
-# setting up syncthing
-echo "Setting up syncthing"
-
-#!/bin/bash
-
-# Start Syncthing in the background
-nohup syncthing &>/dev/null &
-
-# Enable Syncthing to start at boot
-if command -v systemctl &>/dev/null; then
-    systemctl --user enable syncthing.service
-    systemctl --user start syncthing.service
-else
-    echo "Systemd not found. Ensure Syncthing starts manually or via rc.local."
-fi
-
-echo "Syncthing setup complete."
